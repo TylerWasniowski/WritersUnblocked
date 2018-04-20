6e0161372309dd02d90460db5399fa6f580e10ed
TylerWasniowski <TylerWasniowski@gmail.com>
2018-04-20 16:33:54 -0700

    Added query param

--- writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
+++ writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
@@ -1,7 +1,9 @@
 defmodule WritersUnblockedWeb.StoryController do
   use WritersUnblockedWeb, :controller
+  require Logger
 
-  def index(conn, _params) do
+  def index(conn, params) do
+    Logger.info elem(Map.fetch(params, "action"), 1)
     render conn, "index.html"
   end
 end
