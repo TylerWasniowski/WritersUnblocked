9a590426ef8cb343b7f2cb4ba796547def352898
TylerWasniowski <TylerWasniowski@gmail.com>
2018-04-29 16:17:06 -0700

    Moved session creation to story

--- writers_unblocked/lib/writers_unblocked_web/controllers/home_controller.ex
+++ writers_unblocked/lib/writers_unblocked_web/controllers/home_controller.ex
@@ -1,25 +1,8 @@
 defmodule WritersUnblockedWeb.HomeController do
   use WritersUnblockedWeb, :controller
-  alias WritersUnblocked.Repo
-  alias WritersUnblocked.Session
   require Logger
 
   def index(conn, _params) do
-    if get_session(conn, :session_id) do
       render conn, "index.html"
-    else
-      # Make a new session in the database, and save the id
-      session_id = %Session{}
-      |> Repo.insert
-      |> elem(1)
-      |> Map.fetch(:id)
-      |> elem(1)
-
-      Logger.debug "Created session with id: #{session_id}"
-
-      conn
-      |> put_session(:session_id, session_id)
-      |> render("index.html")
-    end
   end
 end
--- writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
+++ writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
@@ -1,12 +1,31 @@
 defmodule WritersUnblockedWeb.StoryController do
   use WritersUnblockedWeb, :controller
+  alias WritersUnblocked.Repo
+  alias WritersUnblocked.Session
   require Logger
 
   def index(conn, params) do
+    if get_session(conn, :session_id) do
+      render conn, "index.html"
+    else
+      # Make a new session in the database, and save the id
+      session_id = %Session{}
+      |> Repo.insert
+      |> elem(1)
+      |> Map.fetch(:id)
+      |> elem(1)
+
+      Logger.debug "Created session with id: #{session_id}"
+
+      conn
+      |> put_session(:session_id, session_id)
+      |> render("index.html")
+    end
+
     params
     |> Map.fetch("action")
     |> elem(1)
-    |> Logger.info
+    |> Logger.debug
 
     conn
     |> get_session(:session_id)
