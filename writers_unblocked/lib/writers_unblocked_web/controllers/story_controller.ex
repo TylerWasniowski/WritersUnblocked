085609ceff9e3b7cf8cb52e207e3de25bb221c26
stephenkairos <stephenkairos.reyes@sjsu.edu>
2018-04-29 22:53:38 -0700

    added function to story_controller

--- writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
+++ writers_unblocked/lib/writers_unblocked_web/controllers/story_controller.ex
@@ -71,7 +71,19 @@ defmodule WritersUnblockedWeb.StoryController do
       byte_size(input) == 0 ->
         text conn, "No form data."
       String.printable?(input) ->
-        text conn, "You clicked the update story button with form input: " <> input
+        aquery = from s in "stories", where: s.title == ^storyid, select: s.body # makes a query statement, similar to SQL
+        astory = WritersUnblocked.Repo.all(aquery) # Queries the database, from the "stories" table
+
+        storyitem = List.first(astory) # gets the story text from said item
+
+        if (storyitem == nil) do # checks if the story is already in the database
+          newstory = %WritersUnblocked.Story{title: storyid, body: input}
+          WritersUnblocked.Repo.insert(newstory)
+          text conn, "Added Story to Database. You clicked the update story button with form input: " <> input
+        else
+          text conn, "Found something: " <> storyitem
+        end
+        text conn, "testing printable"
       true ->
         text conn, "Data contains non-printable chars."
     end
