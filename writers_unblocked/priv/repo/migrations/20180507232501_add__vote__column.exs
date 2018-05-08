0ade1656f6619cac6b18f0929a0104a7f2a77f24
TylerWasniowski <TylerWasniowski@gmail.com>
2018-05-07 18:18:35 -0700

    Fixed votes migration

--- writers_unblocked/priv/repo/migrations/20180507232501_add__vote__column.exs
+++ writers_unblocked/priv/repo/migrations/20180507232501_add__vote__column.exs
@@ -2,6 +2,8 @@ defmodule WritersUnblocked.Repo.Migrations.Add_Vote_Column do
   use Ecto.Migration
 
   def change do
-
+    alter table(:stories) do
+      add(:votes, :integer, default: 0, null: false)
+    end
   end
 end
